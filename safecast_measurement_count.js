53719d10c0637cba5f591695be7af94eaf9e6e2e
rknLA <kevin@rkn.la>
2012-08-03 16:55:29 -0700

    Refactor code a bit, add safecast namespace.

--- safecast_measurement_count.js
+++ safecast_measurement_count.js
@@ -1,42 +1,48 @@
-//TODO check for the existence of Safecast variable and add method without killing other methods
-
-
-var Safecast = {
-  getMeasurementCount: function (element_id) {
-    if (typeof(element_id) != 'string') {
-      alert('Safecast.getMeasurementCount() called with non-string argument');
-      return;
-    }
-
-    var xmlhttp;
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-      xmlhttp=new XMLHttpRequest();
-    }
-    else
-    {// code for IE6, IE5
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-
-    xmlhttp.open("GET", 'https://api.safecast.org/api/measurements/count', true);
-    xmlhttp.setRequestHeader('Accept', 'application/json');
-
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        //request is done and good
-        var jsonResponse = JSON.parse(xmlhttp.responseText);
-        var count = jsonResponse.count;
-
-        var destination = document.getElementById(element_id);
-        if (destination) {
-          destination.innerHTML = count;
-        }
-      }
-    };
-
-    xmlhttp.send();
-
-  },
+if (window.safecast != undefined) {
+  console.log('WARNING! Variable \'safecast\' is already defined!');
+} else {
+  window.safecast = {};
 }
+
+if (safecast.getMeasurementCount != undefined) {
+  console.log('WARNING! safecast.getMeasurementCount is already defined and is about to be overwritten!');
+}
+safecast.getMeasurementCount = function (element_id) {
+  if (element_id != undefined && typeof(element_id) != 'string') {
+    alert('Safecast.getMeasurementCount() called with non-string argument');
+    return;
+  }
+
+  var xmlhttp;
+  if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  }
+  else
+  {// code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  xmlhttp.open("GET", 'https://api.safecast.org/api/measurements/count', true);
+  xmlhttp.setRequestHeader('Accept', 'application/json');
+
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      //request is done and good
+      var jsonResponse = JSON.parse(xmlhttp.responseText);
+      var count = jsonResponse.count;
+
+      var destination = document.getElementById(element_id);
+      if (destination) {
+        destination.innerHTML = count;
+      } else {
+        console.log(count + ' measurements');
+      }
+    }
+  };
+
+  xmlhttp.send();
+
+};
 
 
